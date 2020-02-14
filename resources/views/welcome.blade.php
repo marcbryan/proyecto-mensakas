97ad27d4615516252629f2bf3385ef1ead086363
rayaalmudena <36917373+rayaalmudena@users.noreply.github.com>
2020-02-14 20:04:07 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,5 @@
 @extends('layouts.app')
 
-@section('style')
-#fondoPrincipal {
-* The image used */
-  background-image: url("../../images/branding/fondo1.jpeg");
-
-  /* Full height */
-  height: 100%;
-
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-@endsection
-
 @section('content')
  
 @endsection
